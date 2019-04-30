a41179c8ac3f664809fd87c1792197128e84dc26
sjdeak <sjdeak@yahoo.com>
2019-04-30 16:33:24 +0800

    findPeakElement: 思路想复杂了 判断一侧即可让循环范围缩小

--- binary-search-class/Find_Peak_Element.py
+++ binary-search-class/Find_Peak_Element.py
@@ -26,18 +26,23 @@ class Solution(object):
     """
     length = len(nums)
     if length < 3:
-      return max(nums)
+      return nums.index(max(nums))
 
     left, right = 0, length - 1
     while left + 2 <= right:
       mid = left + (right - left) // 2
       print('left, mid, right:', left, mid, right)
-      if nums[mid] > nums[left] and nums[mid] > nums[mid + 1]:
+      if nums[mid] > nums[mid - 1]:
         right = mid
-      elif nums[mid] > nums[right] and nums[mid] > nums[mid - 1]:
-        left = mid
+      else:
+        right = mid - 1
 
-    return max(nums[left], nums[right])
+      if nums[mid] > nums[mid + 1]:
+        left = mid
+      else:
+        left = mid + 1
+
+    return left if nums[left] > nums[right] else right
 
 
 if __name__ == '__main__' and ('SJDEAK' in os.environ):
@@ -49,6 +54,7 @@ if __name__ == '__main__' and ('SJDEAK' in os.environ):
     print('结果: ', Solution().findPeakElement(*args), end='\n-----\n')
 
 
+  test([1, 2])
   test([1, 2, 3, 1])
   test([1, 2, 1, 3, 5, 6, 4])
 else:
